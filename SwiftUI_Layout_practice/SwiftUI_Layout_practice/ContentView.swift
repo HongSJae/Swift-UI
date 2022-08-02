9975dedee30ae4ffaec8f619c149ddd3f1871d7f
hsj._.06 <gtw030488@gmail.com>
2022-08-02 18:28:14 +0900

    SwiftUI - List

--- SwiftUI_Layout_practice/SwiftUI_Layout_practice/ContentView.swift
+++ SwiftUI_Layout_practice/SwiftUI_Layout_practice/ContentView.swift
@@ -8,62 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State
+    var isNavigationbarHidden = false
+    
     var body: some View {
         
-        ZStack(alignment: .bottomTrailing) {
-            VStack(alignment: .leading, spacing: 0) {
-                HStack {
-                    Image(systemName: "line.horizontal.3")
-                        .font(.largeTitle)
-                    Spacer()
-                    Image(systemName: "person.crop.circle.fill")
-                        .font(.largeTitle)
-                }
-                .padding(20)
-                Text("Reswo._.의 할 일목록")
-                    .font(.system(size: 40))
-                    .fontWeight(.black)
-                    .padding(.horizontal, 20)
-                    .padding(.bottom, 20)
-                ScrollView {
-                    VStack {
-                        MyProjectCard()
-                        MyBasicCard()
-                        MyCard(icon: "tray.fill",
-                               title: "방 청소",
-                               Start: "10 AM",
-                               End: "11 AM",
-                               bgColor: .gray)
-                        MyCard(icon: "tv.fill",
-                               title: "유튜브 보기",
-                               Start: "3 PM",
-                               End: "5 PM",
-                               bgColor: .red)
-                        MyCard(icon: "gamecontroller.fill",
-                               title: "롤 한 판하기",
-                               Start: "12 AM",
-                               End: "2 AM",
-                               bgColor: .orange)
-                        MyCard(icon: "phone.fill",
-                               title: "전화 하기",
-                               Start: "미정",
-                               End: "미정",
-                               bgColor: .blue)
+        NavigationView {
+            ZStack(alignment: .bottomTrailing) {
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack {
+                        NavigationLink(destination: MyList(isNavigationBarHidden: self.$isNavigationbarHidden)) {
+                            Image(systemName: "line.horizontal.3")
+                                .font(.largeTitle)
+                                .foregroundColor(.black)
+                        }
+                        Spacer()
+                        Image(systemName: "person.crop.circle.fill")
+                            .font(.largeTitle)
                     }
-                    .padding()
-                } //ScrollView
-            } //VStack
-           
-            
-            Circle()
-                .foregroundColor(.yellow)
-                .frame(width: 60, height: 60)
-                .overlay(
-                    Image(systemName: "plus")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white))
-                .padding(.trailing, 10)
-                .shadow(radius: 20)
+                    .padding(20)
+                    Text("Reswo._.의 할 일목록")
+                        .font(.system(size: 40))
+                        .fontWeight(.black)
+                        .padding(.horizontal, 20)
+                        .padding(.bottom, 20)
+                    ScrollView {
+                        VStack {
+                            MyProjectCard()
+                            MyBasicCard()
+                            MyCard(icon: "tray.fill",
+                                   title: "방 청소",
+                                   Start: "10 AM",
+                                   End: "11 AM",
+                                   bgColor: .gray)
+                            MyCard(icon: "tv.fill",
+                                   title: "유튜브 보기",
+                                   Start: "3 PM",
+                                   End: "5 PM",
+                                   bgColor: .red)
+                            MyCard(icon: "gamecontroller.fill",
+                                   title: "롤 한 판하기",
+                                   Start: "12 AM",
+                                   End: "2 AM",
+                                   bgColor: .orange)
+                            MyCard(icon: "phone.fill",
+                                   title: "전화 하기",
+                                   Start: "미정",
+                                   End: "미정",
+                                   bgColor: .blue)
+                        }
+                        .padding()
+                    } //ScrollView
+                } //VStack
+                
+                
+                Circle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 60, height: 60)
+                    .overlay(
+                        Image(systemName: "plus")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white))
+                    .padding(.trailing, 10)
+                    .shadow(radius: 20)
+            }
+            .navigationTitle("메인")
+            .navigationBarHidden(self.isNavigationbarHidden)
+            .onAppear {
+                self.isNavigationbarHidden = true
+            }
         }
     }
 }
---
+++ SwiftUI_Layout_practice/SwiftUI_Layout_practice/Preview Content/MyList.swift
@@ -0,0 +1,68 @@
+//
+//  MyList.swift
+//  SwiftUI_Layout_practice
+//
+//  Created by 홍승재 on 2022/08/02.
+//
+
+import SwiftUI
+
+struct MyList: View {
+    
+    @Binding
+    var isNavigationBarHidden: Bool
+    
+    init(isNavigationBarHidden: Binding<Bool> = .constant(false)) {
+        _isNavigationBarHidden = isNavigationBarHidden
+    }
+    
+    var body: some View {
+        
+        List {
+            
+            Section(header: Text("오늘 할 일")
+                .font(.headline)
+                .foregroundColor(.black),
+            footer: Text("footer")) {
+                ForEach(1...3, id: \.self) {
+    //                Text("MyList \($0)")
+                    MyCard(icon: "gamecontroller.fill",
+                           title: "롤 \($0)판하기",
+                           Start: "12 AM",
+                           End: "2 AM",
+                           bgColor: .red)
+                }
+            }
+            .listRowInsets(EdgeInsets.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+            
+            Section(header: Text("내일 할 일")
+                .font(.headline)
+                .foregroundColor(.black),
+            footer: Text("footer")) {
+                ForEach(1...3, id: \.self) {
+    //                Text("MyList \($0)")
+                    MyCard(icon: "gamecontroller.fill",
+                           title: "롤 \($0)판하기",
+                           Start: "12 AM",
+                           End: "2 AM",
+                           bgColor: .orange)
+                }
+            }
+            .listRowInsets(EdgeInsets.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+            .listRowBackground(Color.yellow)
+        }
+        .listStyle(.grouped)
+        .navigationTitle("내 목록")
+//        .navigationBarHidden(self.isNavigationbarHidden)
+        .onAppear {
+            self.isNavigationBarHidden = false
+        }
+    }
+}
+
+struct MyList_Previews: PreviewProvider {
+    static var previews: some View {
+        MyList()
+            .previewInterfaceOrientation(.portrait)
+    }
+}
