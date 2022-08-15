8b108bf4356ec5547b8b867a676add4711dd1c03
hsj._.06 <gtw030488@gmail.com>
2022-08-15 23:43:57 +0900

    CMD update

--- CMD to SwiftUI/CMD to SwiftUI/TabBar/Detail_Info.swift
+++ CMD to SwiftUI/CMD to SwiftUI/TabBar/Detail_Info.swift
@@ -7,23 +7,36 @@
 
 import SwiftUI
 
+struct bginfo {
+    // create static variables for custom colors
+    static let BackgroundColor = Color("Classinfo")
+    
+    //... add the rest of your colors here
+}
+
 struct Detail_Info: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
-        ZStack(alignment: .top) {
-            ColorManager.BackgroundColor.ignoresSafeArea()
-            VStack {
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Image("x")
-                            .resizable()
-                            .frame(width: 16, height: 16)
-                            .padding(20)
+        GeometryReader { proxy in
+            ZStack(alignment: .top) {
+                bginfo.BackgroundColor.ignoresSafeArea()
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image("x")
+                                .resizable()
+                                .frame(width: 16, height: 16)
+                                .padding(20)
+                        }
                     }
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: proxy.size.width / 5, height: proxy.size.width / 5)
+                    Text("")
                 }
             }
         }
